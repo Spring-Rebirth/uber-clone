91e3dddecaca1e0108461611b6f2e9a5dd437423
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-05 06:41:10 +0800

    暂存

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,12 +1,20 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
 
 export default function Onboarding() {
   return (
       <SafeAreaView className="h-full items-center ">
-         <TouchableOpacity className='w-full justify-end items-end p-5'>
+         <TouchableOpacity 
+            className='w-full justify-end items-end p-5'
+            onPress={() => {
+               router.replace('/(auth)/sign-up');
+            }}
+         >
+
             <Text className="font-JakartaBold">Skip</Text>
+            
          </TouchableOpacity>
       </SafeAreaView>
   );
