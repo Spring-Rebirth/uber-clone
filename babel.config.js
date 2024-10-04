5f5ee233efc470143356342c358c36c6ad6adfc9
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-04 19:49:45 +0800

    完成NativeWind集成

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
-module.exports = function (api) {
+// cSpell:words nativewind
+import { plugins } from "./tailwind.config";
+
+export default function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: ["nativewind/babel"],
   };
 };
